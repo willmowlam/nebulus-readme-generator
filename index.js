ffd229960dcc62263380c7dd0b8e1dd24655d4c5
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-02-04 17:43:38 +0000

    removed licenses package; added license templates; added prompt defaults

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
-const license = require('license');
 
 // Promisify fs.writeFile() to use promises
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// The target filename
-const targetFile = "output/README.md";
+// Promisify fs.readFile() to use promises
+const readFileAsync = util.promisify(fs.readFile);
+
+// The target readme filename
+const targetReadmeFile = "./output/README.md";
 
 // Function for prompting user for contents of README
 const promptUser = () =>
@@ -20,126 +22,138 @@ const promptUser = () =>
       type: 'input',
       name: 'title',
       message: 'What is your project Title?',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'Project Title'
     },
 
     {
       type: 'input',
       name: 'description',
       message: 'Provide a short Description explaining the project; eg what was the motivation, what problem does it solve?',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'This project is designed to...'
     },
 
     {
       type: 'input',
       name: 'installation',
       message: 'What are the Installation instructions?',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'git clone ...'
     },
 
     {
       type: 'input',
       name: 'usage',
       message: 'What are the Usage instructions?',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'open terminal, run node index.js'
     },
 
     {
       type: 'input',
       name: 'liveUrl',
       message: 'What is the URL of the live application (optional)?',
+      default: 'https://www.example.com'
     },
 
     {
       type: 'input',
       name: 'screenshotUrl',
-      message: 'Provide a link to a Screenshot of the application (optional)',
+      message: 'Provide the local path or URL to a Screenshot of the application (optional)',
+      // Validate the screenshot file exists?
+      default: './assets/images/screenshot.jpg'
     },
 
     {
       type: 'input',
       name: 'credits',
       message: 'Enter any Credits (optional).',
+      default: 'J Bloggs, A Nother'
     },
     
     {
       type: 'input',
       name: 'features',
       message: 'Enter the project Features (optional).',
+      default: 'This app has features X and Y.'
     },
     
 
-    // Is this a open source project?
-      // If yes; 
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'Please provide Contribution guidelines (optional).',
-      },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Please provide Contribution guidelines (optional).',
+      default: 'Feel free to contribute to the project by submitting a pull request.'
+    },
 
-    // Do you have any tests included with your application? 
-      // If yes;
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter the commands users need to run the Tests (optional).',
-      },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter the commands users need to run the Tests (optional).',
+      default: 'node test.js'
+    },
 
     {
       type: 'input',
       name: 'githubUsername',
       message: 'For the Questions section, enter your GitHub username.',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'myGitHubUsername'
     },
 
     {
       type: 'input',
       name: 'emailAddress',
       message: 'For the Questions section, enter your email address.',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'me@example.com'
     },
 
-    // Would you like to add badges to the top of the README?
-      // If yes, select which type(s)? [GitHub, npm]
-      {
-        type: 'input',
-        name: 'badges',
-        message: 'What badges would you like to the top of the README (optional)?',
-      },
+    {
+      type: 'input',
+      name: 'badges',
+      message: 'What badges would you like to the top of the README (optional)?',
+      default: '![nutriSmartApp](https://img.shields.io/github/languages/top/willmowlam/nutriSmartApp)'
+    },
 
-      // MIT, GNU, Creative Commons, Apache 2.0, BSD-2-Clause
-      {
-        type: 'list',
-        name: 'license',
-        message: 'What is the project License?',
-        choices: [
-          {
-            name: '1) MIT License\nPermission for commercial/private use, distribution and modification. Must have License and Copyright notices. Limited liability and no warranty.',
-            value: '[MIT License](https://spdx.org/licenses/MIT.html)',
-          },
-          {
-            name: '2) Apache-2.0 License\nAs MIT, but allows patent use and requires documentation of changes. No trademark use without explicit permission.',
-            value: '[Apache-2.0 License](http://www.apache.org/licenses/)',
-          },
-          {
-            name: '3) GNU General Public License v3.0\nAs MIT and Apache but derivative works must also be distributed under the same license and source code made available. No explicit trademark limitation.',
-            value: '[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)',
-          },
-        ]
-      },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a License?',
+      choices: [
+        {
+          name: '1) MIT License\nPermission for commercial/private use, distribution and modification. Must have License and Copyright notices. Limited liability and no warranty.',
+          value: 'MIT',
+        },
+        {
+          name: '2) Apache-2.0 License\nAs MIT, but allows patent use and requires documentation of changes. No trademark use without explicit permission.',
+          value: 'Apache-2.0',
+        },
+        {
+          name: '3) GNU General Public License v3.0\nAs MIT and Apache but derivative works must also be distributed under the same license and source code made available. No explicit trademark limitation.',
+          value: 'GPL-3.0',
+        },
+      ],
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+      default: 'MIT'
+    },
 
-/*
-      MIT License
-      Permission for commercial/private use, distribution and modification. Must have License and Copyright notices. Limited liability and no warranty.
+    {
+      type: 'input',
+      name: 'legalName',
+      message: 'What is your Legal Name, to be shown on the License?',
+      validate: value => value.trim() ? true : 'This is required in order to build the README. Press CTRL+C to cancel.',
+    //  default: 'Joe Bloggs'
+    },
 
-      Apache License 2.0
-      As MIT, but allows patent use and requires documentation of changes. No trademark use without explicit permission.
-
-      GNU GPLv3
-      As MIT and Apache but derivative works must also be distributed under the same license and source code made available. No explicit trademark limitation.
-*/
-
-    ]);
+  ]);
 
 
 // Function to generate markdown
 const generateMarkdown = (answers) => 
 `
-[badges]
+${answers.badges}
 
 # ${answers.title}
 
@@ -203,7 +217,9 @@ ${answers.questions}
 
 ## License
 
-This project is licensed under the ${answers.license}.
+![GitHub License](https://img.shields.io/github/license/:user/:repo)
+
+${answers.licenseText}
 
 `;
 
@@ -222,14 +238,84 @@ This project is licensed under the ${answers.license}.
     // Questions
     // License
 
+
+// Translate the tokens in the license template
+const getLicenseText = (answers, template) => {
+
+  // Retrieve license text based on the selected license
+  let licenseText = template;
+
+  // Throw error if unable to get license
+  if (typeof licenseText !== 'string' || licenseText.length === 0) {
+      throw new Error('Failed to retrieve license text');
+  }
+
+  // Change any <year> to current year
+  const currentYear = new Date().getFullYear();
+
+  switch (answers.license){
+    case "MIT":
+      // Update placeholder for current year   
+      licenseText = licenseText.replace(/\[year\]/g, `${currentYear}`);
+
+      // Update placeholder for the legal owner
+      licenseText = licenseText.replace(/\[fullname\]/g, `${answers.legalName}`);
+
+      break;
+    case "Apache-2.0":
+      // Update placeholder for current year  
+      licenseText = licenseText.replace(/\[yyyy\]/g, `${currentYear}`);
+
+      // Update placeholder for the legal owner
+      licenseText = licenseText.replace(/\[name of copyright owner\]/g, `${answers.legalName}`);
+
+      break;
+
+    case 'GPL-3.0':
+      // Update placeholder for current year   
+      licenseText = licenseText.replace(/<year>/g, `${currentYear}`);
+
+      // Update placeholder for current year   
+      licenseText = licenseText.replace(/<program>/g, `${answers.title}`);
+        
+      // Update placeholder for the legal owner
+      licenseText = licenseText.replace(/<name of author>/g, `${answers.legalName}`);
+
+      // Change program name to project title
+      licenseText = licenseText.replace(/<one line to give the program\'s name and a brief idea of what it does.>/g,  `${answers.description}`);
+      
+      break;
+  }
+
+  // Add the license text to the answers object
+  answers.licenseText = licenseText;
+
+  return answers;
+}
+
 // Call the prompt function to gather user input and create markdown README file using promisify to asynchronously write the file. Log any errors.
 promptUser()
+
+  // Get license template
+  .then(async (answers) => {
+
+    const licenseTemplate = await readFileAsync(`./assets/licenses/${answers.license}`, 'utf8');
+    return {answers, licenseTemplate};
+
+  })
   
-   // Write markdown file from answers
-  .then((answers) => writeFileAsync(targetFile, generateMarkdown(answers)))
+  // Get the license
+  .then(( {answers, licenseTemplate} ) => {   
+
+    return getLicenseText(answers, licenseTemplate);
+
+  })
+  
+  // Write markdown file from answers
+  .then((answers) => writeFileAsync(targetReadmeFile, generateMarkdown(answers)))
 
   // Log success
-  .then(() => console.log(`Successfully wrote to ${targetFile}`))
+  .then(() => console.log(`Successfully wrote to ${targetReadmeFile}`))
 
   // Catch errors and write to console
   .catch((err)=> console.error(err));
